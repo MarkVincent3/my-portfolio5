ae29d673d4477a7248a6dc2ba7f4f8fdf97d5777
MarkVincent3 <153539994+MarkVincent3@users.noreply.github.com>
2023-12-12 21:35:14 +0800

    script.js

--- script.js
+++ script.js
@@ -8,46 +8,7 @@ const agents = [
       "The opponent played cautiously, often relies on teammates utilities to secure kills."
     ]
   },
-  // ... other agents
-];
-
-let currentAgent;
-let attempts = 3;
-
-function startGame() {
-  let randomAgentIndex = Math.floor(Math.random() * agents.length);
-  currentAgent = agents[randomAgentIndex];
-  attempts = 3;
-
-  displayClues();
-}
-
-function displayClues() {
-  let cluesDiv = document.getElementById("clues");
-  cluesDiv.innerHTML = "<h2>Clues:</h2>";
-  currentAgent.clues.forEach((clue) => {
-    cluesDiv.innerHTML += "<p>" + clue + "</p>";
-  });
-}
-
-function checkGuess() {
-  let guess = document.getElementById("guess").value.toLowerCase();
-  let feedback = document.getElementById("feedback");
-
-  if (guess === currentAgent.agent.toLowerCase()) {
-    feedback.innerHTML = "Congratulations! You guessed correctly. It was " + currentAgent.agent + "!";
-  } else {
-    attempts--;
-    if (attempts > 0) {
-      feedback.innerHTML = "Incorrect guess. Try again! Attempts left: " + attempts;
-    } else {
-      feedback.innerHTML = "Out of attempts! The undisclosed agent was " + currentAgent.agent + ".";
-    }
-  }
-}
-// Starting the game on page load
-window.onload = startGame;
-{
+  {
   "roundNumber": 1,
   "attemptsLeft": 3,
   "opponent": {
@@ -95,3 +56,43 @@ window.onload = startGame;
   },
   // Add more agents and clues as needed
 ]
+  // ... other agents
+];
+
+let currentAgent;
+let attempts = 3;
+
+function startGame() {
+  let randomAgentIndex = Math.floor(Math.random() * agents.length);
+  currentAgent = agents[randomAgentIndex];
+  attempts = 3;
+
+  displayClues();
+}
+
+function displayClues() {
+  let cluesDiv = document.getElementById("clues");
+  cluesDiv.innerHTML = "<h2>Clues:</h2>";
+  currentAgent.clues.forEach((clue) => {
+    cluesDiv.innerHTML += "<p>" + clue + "</p>";
+  });
+}
+
+function checkGuess() {
+  let guess = document.getElementById("guess").value.toLowerCase();
+  let feedback = document.getElementById("feedback");
+
+  if (guess === currentAgent.agent.toLowerCase()) {
+    feedback.innerHTML = "Congratulations! You guessed correctly. It was " + currentAgent.agent + "!";
+  } else {
+    attempts--;
+    if (attempts > 0) {
+      feedback.innerHTML = "Incorrect guess. Try again! Attempts left: " + attempts;
+    } else {
+      feedback.innerHTML = "Out of attempts! The undisclosed agent was " + currentAgent.agent + ".";
+    }
+  }
+}
+// Starting the game on page load
+window.onload = startGame;
+
